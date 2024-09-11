a4e309e5d50fc4e966b39ae1d632aa10df7055c2
Rukaiia <raimberdievarukaya@gmail.com>
2024-09-11 21:56:04 +0600

    did 65 hw

---
+++ src/main/java/com/example/jobsearch/config/LocaleConfig.java
@@ -0,0 +1,23 @@
+package com.example.jobsearch.config;
+
+import org.hibernate.Interceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+@Configuration
+public class LocaleConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+       registry.addInterceptor(localeInterceptor());
+    }
+
+    private LocaleChangeInterceptor localeInterceptor() {
+
+        var localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+}
